163aa70387af1c079643033e91422da916fc9bf9
unknown <edwincruz255@gmail.com>
2021-05-13 16:49:24 -0600

    added new releases

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NewReleases from "./NewReleases";
+import Recommendations from "./Recommendations";
 
 const Dashboard = () => {
   return (
     <div>
       <NewReleases />
+      {/* <Recommendations /> */}
     </div>
   );
 };
