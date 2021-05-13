53696b678a4011c4968e3bcaaf71560cef3e9375
unknown <edwincruz255@gmail.com>
2021-05-13 15:35:08 -0600

    added login, and new releases

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import dashboardReducer from "../features/dashboard/dashbaordSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    myLibrary: dashboardReducer,
   },
 });
